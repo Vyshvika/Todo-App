2497be7525472ec124e1852d50e1a5445495b762
vyshvika <kotagirivyshvika@gmail.com>
2024-04-20 17:31:05 +0530

    changes

--- function.py
+++ function.py
@@ -9,7 +9,7 @@ def get_todos(filepath="todos.txt"):
     return todos_local
 
 
-def write_todos(todos_arg,filepath="todos.txt"):
+def write_todos(todos_arg, filepath="todos.txt"):
     """ Write the to-do items list in the text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
